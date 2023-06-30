a357e86f927b5a64fd6d2ac1b1cbe1d9e078b92e
dev <dev>
2023-06-30 16:09:38 +0200

    CalculatorView vollständig implementiert.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import view.CalculatorView;
+
+import javax.swing.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
+//        // Das ist nicht best practices:
+        // new CalculatorView();
 
+//        // Functional Interface runnable hat nur eine abstrakte Methode.
+//        // Fügt unsere Anwendung dem UI Thread hinzu. Die Anwendung wird aufgerufen, sobald alle Events der Eventqueue
+//        // abgearbeitet wurden.
+        // Hier implementieren wir das Runnable Interface in eine anonyme Klasse:
+//        SwingUtilities.invokeLater(new Runnable()
+//        {
+//            @Override
+//            public void run()
+//            {
+//                new CalculatorView();
+//            }
+//        });
+
+        // Alternative: wir implementieren runnable als Lambdaausdruck:
+        SwingUtilities.invokeLater(() -> new CalculatorView());
     }
 }
---
+++ src/view/CalculatorView.java
@@ -0,0 +1,38 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorView
+{
+    // Attribute, Instanzvariablen, Felder:
+    private JFrame calculatorFrame;
+    private JTextField txt_display;
+    private JPanel pnl_buttons;
+
+    public CalculatorView()
+    {
+        this.calculatorFrame = new JFrame();
+        this.calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Feste Werte im Quellcode ist ein Code Smell!
+        // Hier haben Pixelwerte hardcoded. Solche fest in den quellcode eingetragene Werte werden als Literale
+        // bezeichnet. Oder auch einfach Magic Numbers.
+        this.calculatorFrame.setSize(420, 550);
+
+//        this.calculatorFrame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,
+//                Toolkit.getDefaultToolkit().getScreenSize().height);
+
+        this.calculatorFrame.setMinimumSize(new Dimension(400, 200));
+        this.calculatorFrame.setPreferredSize(new Dimension(400, 200));
+
+        // bad practice:
+        this.calculatorFrame.setLayout(null);
+        this.calculatorFrame.setLocationRelativeTo(null);
+
+        this.calculatorFrame.getContentPane().setBackground(new Color(104, 139, 173));
+        this.calculatorFrame.setTitle("CALCUTRON 5000");
+
+        this.calculatorFrame.setVisible(true);
+    }
+}
